d134bdbc67a119a5cd023d5719b7391c3a174349
aniket1702 <aniketmaurya1702@gmail.com>
2022-10-04 17:22:08 +0530

    Added constant for XML-config file extent report

--- src/main/java/org/anytest/constants/Constants.java
+++ src/main/java/org/anytest/constants/Constants.java
@@ -3,16 +3,21 @@ package org.anytest.constants;
 import org.anytest.utils.config.ConfigFactory;
 import org.anytest.utils.dateutils.DateUtils;
 
+
 public final class Constants {
     private Constants(){}
 
+    /* CONSTANTS DEFINED FOR GETTING FILES FROM RESOURCES FOLDER */
     private static final String BASE_PATH = System.getProperty("user.dir");
     private static final String EXTENT_REPORT_FOLDER_PATH = BASE_PATH + "/test-output/sparkReport/";
     private static String extentReportFilePath = "";
+    private static final String EXTENT_REPORT_XML_PATH = BASE_PATH+"/src/main/resources/extent-reporter/extent-config.xml";
 
+    /* METHOD TO CREATE EXTENT REPORT HTML FILE. CREATES NEW FOLDER WITH CURRENT DATE AND TIME IF OVERRIDE REPORT IS SELECTED AS NO. OTHERWISE, IT OVERRIDES THE REPORT.*/
     public static String getExtentReportFolderPath()
     {
 
+        /* GETS THE CURRENT DATE AND TIME FROM DATEUTIL CLASS FROM UTIL PACKAGE */
         String newDateTimeFormat= DateUtils.getCurrentDateTime();
 
         return (ConfigFactory.getConfig().overrideReport().equalsIgnoreCase("no"))
@@ -21,12 +26,18 @@ public final class Constants {
     }
 
 
+    /* METHOD TO RETURN EXTENT REPORT FILE PATH. */
     public static String getExtentReportFilePath() {
         if(extentReportFilePath.isEmpty())
         {
             extentReportFilePath=getExtentReportFolderPath();
         }
         return extentReportFilePath;
+    }
+
+    /* METHOD TO RETURN EXTENT REPORT XML CONFIG PATH */
+    public static String getExtentReportXMLPath() {
+        return EXTENT_REPORT_XML_PATH;
     }
 
 }
